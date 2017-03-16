44683333ae0d924c3385d286bcfee7f32761c411
Ali-Meysam Mohebbi <alimeysam@gmail.com>
2017-03-15 19:25:13 -0700

    Finalized Ability component

--- src/Components/Abilities.jsx
+++ src/Components/Abilities.jsx
@@ -13,13 +13,13 @@ export default class Abilities extends Component{
 
 
 
-    handleChange(value){
+    handleChange(e, value){
         this.setState({value});
     }
 
     render(){
         let displayAbilities = abilities.map((ability, item) => {
-            return (<MenuItem key={id}
+            return (<MenuItem key={item}
                               value={item}
                               primaryText={ability}/> )
         })
@@ -27,7 +27,7 @@ export default class Abilities extends Component{
             <SelectField
                 floatingLabelText="Ability"
                 value={this.state.value}
-                onChange={this.handleChange()}
+                onChange={this.handleChange(this)}
                 autoWidth={true}
             >
                 {displayAbilities}
