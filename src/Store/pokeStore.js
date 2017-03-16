447b7aecee949d015e47b457c819a350ee95cfdc
robert <rhill352@gmail.com>
2017-03-15 18:57:04 -0700

    created the pokedex array with JSON objects representing the pokemon

--- src/Store/pokeStore.js
+++ src/Store/pokeStore.js
@@ -1,5 +1,7 @@
-const initialState = {
+import pokedex from '../pokedex';
 
+const initialState = {
+    pokemon: pokedex
 };
 const UserStore = (state = initialState, action) => {
     switch (action.type) {
