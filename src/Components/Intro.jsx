e93b488e59d51a95ca27b1ca005b4c6099bbb89d
Alexander Shaw <alexandershaw28@gmail.com>
2017-04-11 09:14:27 -0700

    Hoping to make the Intro page work on IndexContainer

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const style1 = {
-    left: '30%',
+const style = {
     height: 'auto',
-    width: 'auto',
-    textAlign: 'center',
+    width: '75%',
     display: 'inline-block',
     position: 'absolute',
-    zIndex: 5
-};
+    zIndex: '5',
+    borderRadius: '20px',
+    borderColor: 'rgb(0, 188, 212)',
+    borderWidth: '5px'
 
-const displayOff = {
-    display: 'none'
-}
+};
 
 const style2 = {
     margin: 12,
@@ -25,22 +23,26 @@ let isOn = true;
 
 export default class Intro extends React.Component {
 
-    handleClick(){
-        this.isOn = false;
-        render();
+    constructor(props){
+        super(props);
+        this.state = {
+            isOn: true
+        }
     }
-
+    handleClick(){
+        this.setState({isOn: false});
+    }
     render(){
-        if(isOn == true){
+        if(this.state.isOn == true){
         return (
-            <div style={{width: 'auto'}}>
-                <Paper style={style1} zDepth={5}>
-                    <div style={{height: 'auto', width:'auto', }} >
+            <div style={{width: 'auto', marginLeft: '10em' }}>
+                <Paper style={style} zDepth={5}>
+                    <div style={{height: 'auto', width:'auto', padding: '20px' }} >
 
-                        <h2>Welcome to Hatchr! </h2>
-                        <p> This program is designed to guide you through the complex process of Pokemon breeding. <br/>
+                        <h2 style = {{textAlign: 'center'}}>Welcome to Hatchr! </h2>
+                        <p style = {{textAlign: 'left'}}> This program is designed to guide you through the complex process of Pokemon breeding. <br/><br/>
                             To start, you’ll need to select 2 parent Pokemon of different genders that belong to the same
-                            “Egg Group,” keeping in mind that the child will be the same Pokemon species as the mother. <br/>
+                            “Egg Group,” keeping in mind that the child will be the same Pokemon species as the mother. <br/><br/>
                             Now that you know which Pokemon you want, it’s time to look at the Stats. Every Pokemon has the
                             following six stats: Health, Attack, Defense, Special Defense, Special Attack and Speed, each
                             represented by a value from 0 to 31. Pokemon also have abilities, natures, and moves.
@@ -53,7 +55,7 @@ export default class Intro extends React.Component {
 
 
                     </div>
-                    <RaisedButton label="Continue" primary={true} style={style} onClick={this.handleClick} />
+                    <RaisedButton label="Continue" primary={true} style={{float: 'right', marginBottom: '2em', marginRight: '2em'}} onClick={this.handleClick.bind(this)} />
                 </Paper>
 
             </div>
